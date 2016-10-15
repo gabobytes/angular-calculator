e1eb7cd22e78021dac10c6047f3b0ff7d23d2e4a
Gabriel Melo <gabrielmeloarteaga@gmail.com>
2016-10-15 18:06:42 -0500

    keydown numbers

--- js/calculator.js
+++ js/calculator.js
@@ -1,11 +1,17 @@
-app.controller('ctrlCalculator',  function($scope){
-	
+app.controller('ctrlCalculator',  function($scope,$window){
+
+	angular.element($window).on('keydown', function(e){				
+		if(e.keyCode >=48 && e.keyCode <=59){
+			$scope.concatenate(String.fromCharCode(e.keyCode));		
+		}
+	})
+
 	$scope.concatenate = function(value){
 		if($scope.values === 0){
 				$scope.values  = value;
 			}
 		else{				
-				$scope.values = parseInt($scope.values.toString()+value.toString());
+				$scope.values = parseInt($scope.values.toString() + value.toString());
 			}
 	}
 });
